b12000e2675d3420d4be560ea84b1a448e9d77cf
Raihan Stark <raihanyudosaputra30@gmail.com>
2020-12-15 01:32:48 +0700

    add react spring

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Theme from "./Theme";
+
 import styled from "styled-components";
+import { useSpring, animated } from "react-spring";
 
 import Header from "./components/Header/Header";
+
 const Container = styled.div`
   width: 100%;
   text-align: center;
@@ -14,10 +17,18 @@ const Container = styled.div`
 `;
 
 const App = () => {
+  const props = useSpring({
+    transform: "translateY(0px)",
+    from: { transform: "translateY(-10em)" },
+    delay: 500,
+  });
+
   return (
     <Theme>
       <Container>
-        <Header />
+        <animated.div style={props}>
+          <Header />
+        </animated.div>
       </Container>
     </Theme>
   );
